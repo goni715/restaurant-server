001f86f0cd5996856796fed94bea9e7cda4798d0
goni715 <halesgoni715149123@gmail.com>
2025-04-04 21:45:38 +0600

    table interface & model

---
+++ src/app/modules/Table/table.interface.ts
@@ -0,0 +1,10 @@
+import { Types } from "mongoose";
+
+
+export interface ITable {
+    name: string;
+    scheduleId: Types.ObjectId;
+    restaurantId: Types.ObjectId;
+    ownerId: Types.ObjectId;
+    seats: number;
+}
---
+++ src/app/modules/Table/table.model.ts
@@ -0,0 +1,46 @@
+import { model, Schema } from "mongoose";
+import { ITable } from "./table.interface";
+
+
+
+const tableSchema = new Schema<ITable>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    scheduleId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Schedule"
+    },
+    restaurantId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Restaurant",
+    },
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    seats: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value > 0; // Ensures the number is positive
+        },
+        message: "seats must be a positive number",
+      },
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const TableModel = model<ITable>('Table', tableSchema);
+export default TableModel;

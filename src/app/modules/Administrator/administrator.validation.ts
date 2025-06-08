ead87947ccc574235c90451473616f7d0c8c0d50
goni715 <halesgoni715149123@gmail.com>
2025-06-08 17:08:50 +0600

    administrator's validation

--- src/app/modules/Administrator/administrator.validation.ts
+++ src/app/modules/Administrator/administrator.validation.ts
@@ -1,36 +1,57 @@
 import { z } from "zod";
 import { VALID_ACCESS_VALUES } from "./administrator.constant";
-
+import { fullNameRegex } from "../User/user.validation";
 
 export const createAdministratorSchema = z.object({
   administratorData: z.object({
-    fullName: z.string({
-      required_error: "full Name is required",
-    }),
+    fullName: z
+      .string({
+        invalid_type_error: "Full Name must be string",
+        required_error: "full Name is required",
+      })
+      .trim()
+      .regex(fullNameRegex, {
+        message:
+          "fullName can only contain letters, spaces, apostrophes, hyphens, and dots.",
+      }),
     email: z
       .string({
+        invalid_type_error: "email must be string",
         required_error: "email is required",
       })
-      .email(),
+      .trim()
+      .email({
+        message: "Invalid email address",
+      }),
     phone: z.string({
+      invalid_type_error: "phone number must be string",
       required_error: "phone number is required",
     }),
     password: z
       .string({
+        invalid_type_error: "password must be string",
         required_error: "password is required",
       })
-      .min(6, "Password minimum 6 characters long")
       .trim()
+      .min(6, "Password minimum 6 characters long")
       .optional(),
   }),
-  access: z.array(z.enum(VALID_ACCESS_VALUES)).default([]),
+  access: z.array(
+      z.string({
+          invalid_type_error: `access must be ${VALID_ACCESS_VALUES.map(
+            (access) => `'${access}'`
+          ).join(" or ")} `,
+        })
+        .refine((val) => ["user", "owner", "restaurant", "settings"].includes(val), {
+          message: `access must be an array of: 'user', 'owner', 'restaurant', settings.`
+        })
+    )
+    .default([]),
 });
-
 
 export const updateAdministratorAccessSchema = z.object({
-  access: z.array(z.enum(VALID_ACCESS_VALUES)).default([])
+  access: z.array(z.enum(VALID_ACCESS_VALUES)).default([]),
 });
-
 
 export const updateAdministratorSchema = z.object({
   fullName: z.string({
